802a3c903424aa1d03ef11bd4a7c9fcc1c68871f
Swathipriya-kanmani <62537290+Swathipriya-kanmani@users.noreply.github.com>
2022-10-03 16:52:53 +0530

    Add files via upload

---
+++ ChefApp/ChefApp/Controllers/ChefController.cs
@@ -0,0 +1,14 @@
+﻿using ChefApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChefApp.Controllers
+{
+    public class ChefController : Controller
+    {
+        public IActionResult ChefInfo()
+        {
+            List<ChefDetails> che = ChefDetails.ChefInformation();
+            return View(che);
+        }
+    }
+}
---
+++ ChefApp/ChefApp/Models/ChefDetails.cs
@@ -0,0 +1,28 @@
+﻿namespace ChefApp.Models
+{
+    public class ChefDetails
+    {
+        
+        
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public string Dishes { get; set; } 
+
+
+
+
+            public static List<ChefDetails> ChefInformation()
+            {
+                List<ChefDetails> chefs = new() {
+                  new ChefDetails { Id = 1, Name = "Priya" , Dishes = " Pav Bhaji"},
+                  new ChefDetails { Id = 2, Name = "Deepika", Dishes = "  Paneer Butter Masala"},
+                  new ChefDetails { Id = 3, Name = "Divya Bharathi", Dishes = "  Malai Kofta"}
+        };
+                return chefs;
+            }
+       
+    }
+}
+
+
