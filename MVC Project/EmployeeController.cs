5d638688cc498872534785aeb7f4035ca519f041
Swathipriya-kanmani <62537290+Swathipriya-kanmani@users.noreply.github.com>
2022-10-10 22:11:09 +0530

    Add files via upload

---
+++ MVC Project/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SingleEmployeeInfo()
+        {
+            EmployeeDetails emp = EmployeeDetails.GetSingleEmployeeInfo();
+            return View(emp);
+        }
+
+        public IActionResult AllEmployeeInfo()
+        {
+            List<EmployeeDetails> LiEmploy = EmployeeDetails.GetAllEmployeeInfo();
+
+            return View(LiEmploy);
+        }
+
+        public IActionResult Insert()
+        {
+
+            return View(Insert);
+        }
+
+
+    }
+
+    
+
+}
---
+++ MVC Project/EmployeeDetails.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Linq;
+using System.Collections;
+
+namespace EmployeeApp.Models
+{
+    public class EmployeeDetails
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string Gender { get; set; }
+
+
+
+        public static EmployeeDetails GetSingleEmployeeInfo()
+        {
+            EmployeeDetails employee = new()
+            {
+
+
+                id = 1,
+                firstname = "Divya",
+                lastname = "Bharathi",
+                Gender = "Female"
+            };
+
+
+            return employee;
+        }
+
+
+
+        public static List<EmployeeDetails> GetAllEmployeeInfo()
+        {
+
+
+            List<EmployeeDetails> employees = new() {
+                
+                new EmployeeDetails { id = 2, firstname = "Deepika", lastname = "Padukone", Gender = "Female" },
+                new EmployeeDetails { id = 3, firstname = "Swathi", lastname = "Priya", Gender = "Female" },
+                new EmployeeDetails { id = 4, firstname = "Bharathi", lastname = "Raja", Gender = "Male" },
+                new EmployeeDetails { id = 5, firstname = "Hanuman", lastname = "Kesari", Gender = "Male" }
+
+            };
+            return employees;
+
+        }
+    }
+}
